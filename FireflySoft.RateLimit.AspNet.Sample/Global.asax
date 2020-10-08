5f77c3b31c9bd74a2bed7a1b9defaf80dc3dd231
bosima <bosima@126.com>
2020-10-08 16:40:16 +0800

    add asp.net web api rate limit support.

---
+++ FireflySoft.RateLimit.AspNet.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="FireflySoft.RateLimit.AspNet.Sample.Global" %>
---
+++ FireflySoft.RateLimit.AspNet.Sample/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using FireflySoft.RateLimit.Core;
+
+namespace FireflySoft.RateLimit.AspNet.Sample
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // 限流
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new RateLimitHandler(
+                new Core.RateLimitProcessor<HttpRequestMessage>.Builder()
+                .WithError(new Core.RateLimitError()
+                {
+                    Message = "The system is busy, please try again later"
+                })
+                .WithRules(new[] {
+                    new RateLimitRule<HttpRequestMessage>()
+                    {
+                        ExtractTarget = context =>
+                        {
+                             return context.RequestUri.AbsolutePath;
+                        },
+                        CheckRuleMatching = context =>
+                        {
+                            return true;
+                        },
+                        Id=Guid.NewGuid().ToString(),
+                        Name="general limit rule",
+                        LimitNumber=30,
+                        TimeWindow=1
+                    }
+                })
+                .Build()
+             ));
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+}
+}
