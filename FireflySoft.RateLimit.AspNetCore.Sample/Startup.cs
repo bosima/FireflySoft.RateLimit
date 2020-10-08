969ca4738fbbc7c729b3525a6235bddf38207687
bosima <bosima@126.com>
2020-10-08 11:51:27 +0800

    add asp.net core rate limit middleware.

---
+++ FireflySoft.RateLimit.AspNetCore.Sample/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FireflySoft.RateLimit.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FireflySoft.RateLimit.AspNetCore.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRateLimit(new Core.RateLimitProcessor<HttpContext>.Builder()
+                .WithError(new Core.RateLimitError()
+                {
+                    Message = "The system is busy, please try again later"
+                })
+                .WithRules(new [] {
+                    new RateLimitRule<HttpContext>()
+                    {
+                        ExtractTarget = context =>
+                        {
+                             return context.Request.Path.Value;
+                        },
+                        CheckRuleMatching = context =>
+                        {
+                            return true;
+                        },
+                        Id=Guid.NewGuid().ToString(),
+                        Name="general limit rule",
+                        LimitNumber=30,
+                        TimeWindow=1
+                    }
+                })
+                .Build());
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ FireflySoft.RateLimit.AspNetCore/RateLimitMiddleware.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using FireflySoft.RateLimit.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FireflySoft.RateLimit.AspNetCore
+{
+    /// <summary>
+    /// 限流处理中间件
+    /// </summary>
+    public class RateLimitMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly RateLimitProcessor<HttpContext> _processor;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="next"></param>
+        /// <param name="options"></param>
+        /// <param name="loggerFactory"></param>
+        public RateLimitMiddleware(RequestDelegate next, RateLimitProcessor<HttpContext> processor, ILoggerFactory loggerFactory)
+        {
+            if (processor == null)
+            {
+                throw new ArgumentNullException("RateLimitProcessor is null.");
+            }
+
+            _next = next;
+            _loggerFactory = loggerFactory;
+            _processor = processor;
+        }
+
+        /// <summary>
+        /// 中间件的异步处理
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Invoke(HttpContext context)
+        {
+            var checkResult = _processor.Check(context);
+
+            if (checkResult.IsLimit)
+            {
+                context.Response.StatusCode = checkResult.Error.Code;
+
+                if (!string.IsNullOrWhiteSpace(checkResult.Error.Message))
+                {
+                    var bodyContent = Encoding.UTF8.GetBytes(checkResult.Error.Message);
+                    await context.Response.Body.WriteAsync(bodyContent, 0, bodyContent.Length).ConfigureAwait(false);
+                }
+                else
+                {
+                    await context.Response.WriteAsync(string.Empty).ConfigureAwait(false);
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 限流中间件扩展
+    /// </summary>
+    public static class RateLimitMiddlewareExtensions
+    {
+        /// <summary>
+        /// 使用限流处理器
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="processor"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseRateLimit(this IApplicationBuilder builder, RateLimitProcessor<HttpContext> processor)
+        {
+            return builder.UseMiddleware<RateLimitMiddleware>(processor);
+        }
+    }
+}
