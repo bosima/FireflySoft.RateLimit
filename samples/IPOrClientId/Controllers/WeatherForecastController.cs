320da80b99ec1250a37e3eddc4b974b80b5852f3
bosima <bosima@126.com>
2022-07-02 23:51:33 +0800

    Add a demo that limit the rate according to the client IP and client ID.

---
+++ samples/IPOrClientId/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IPOrClientId.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("Future")]
+    public IEnumerable<WeatherForecast> GetFuture()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet("Today")]
+    public WeatherForecast GetToday()
+    {
+        return  new WeatherForecast
+        {
+            Date = DateTime.Now,
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        };
+    }
+}
---
+++ samples/IPOrClientId/Program.cs
@@ -0,0 +1,95 @@
+using FireflySoft.RateLimit.AspNetCore;
+using FireflySoft.RateLimit.Core.InProcessAlgorithm;
+using FireflySoft.RateLimit.Core.Rule;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRateLimit(new InProcessFixedWindowAlgorithm(
+    new[] {
+        new FixedWindowRule()
+        {
+            ExtractTarget = context =>
+            {
+                var httpContext= context as HttpContext;
+
+                // Through CDN
+                var ip = httpContext!.Request.Headers["Cdn-Src-Ip"].FirstOrDefault();
+                if (!string.IsNullOrEmpty(ip))
+                    return ip;
+
+                // Through SLB
+                ip = httpContext!.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+                if (!string.IsNullOrEmpty(ip))
+                    return ip;
+
+                ip = httpContext!.Connection.RemoteIpAddress?.ToString();
+                return ip??"Anonymous-IP";
+            },
+            CheckRuleMatching = context =>
+            {
+                var requestPath = (context as HttpContext)!.Request.Path.Value;
+                if (requestPath == "/WeatherForecast/Future")
+                {
+                    return true;
+                }
+                return false;
+            },
+            Name = "ClientIPRule",
+            LimitNumber = 3,
+            StatWindow = TimeSpan.FromSeconds(1)
+        },
+        new FixedWindowRule()
+        {
+            ExtractTarget = context =>
+            {
+                var httpContext= context as HttpContext;
+                var clientID = httpContext!.Request.Headers["X-ClientId"].FirstOrDefault();
+                if (string.IsNullOrWhiteSpace(clientID))
+                {
+                    clientID=Guid.NewGuid().ToString();
+                    httpContext.Response.Headers.Append("X-ClientId",clientID);
+                }
+
+                return clientID??"Anonymous-ClientId";
+            },
+            CheckRuleMatching = context =>
+            {
+                var requestPath = (context as HttpContext)!.Request.Path.Value;
+                if (requestPath == "/WeatherForecast/Future")
+                {
+                    return true;
+                }
+                return false;
+            },
+            Name = "ClientIdRule",
+            LimitNumber = 1,
+            StatWindow = TimeSpan.FromSeconds(1)
+        }
+    })
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseRateLimit();
+
+app.MapControllers();
+
+app.Run();
